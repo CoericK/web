aa320d53748ec6142b48703415bac3bdf2b0b85b
Kohei Asai <yo@kohei.dev>
2020-04-11 16:06:08 -0700

    Implement index page scaffold (#7)

--- pageComponents/IndexPage/IndexPage.tsx
+++ pageComponents/IndexPage/IndexPage.tsx
@@ -1,8 +1,26 @@
+import { AnchorButton } from "@blueprintjs/core";
+import Link from "next/link";
 import * as React from "react";
 import css from "./IndexPage.module.css";
 
 interface Props extends React.Attributes {}
 
 export default function IndexPage({}: Props) {
-  return <div className={css.root}></div>;
+  return (
+    <main className={css.root}>
+      <h1 className={css.logo}>WeFocus</h1>
+
+      <Link href="/parties/[partyId]" as="/parties/abc" passHref>
+        <AnchorButton icon="plus" large className={css["create-room"]}>
+          Create Party
+        </AnchorButton>
+      </Link>
+
+      <div className={css.help}>
+        <h2>You wanna join to a party?</h2>
+
+        <p>Ask friends an invitation URL and just access to it!</p>
+      </div>
+    </main>
+  );
 }
